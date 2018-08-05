e53219177184f4fb8605947b59414b51690ab8b4
Mark Guan <markguan@uw.edu>
2018-08-05 20:13:53 +0000

    Changed name in index.php to my name

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
 				<!-- Header -->
 					<header id="header">
-						<h1>Adam Jensen</h1>
+						<h1>Mark Guan</h1>
 						<p>Security Chief &nbsp;&bull;&nbsp; Cyborg &nbsp;&bull;&nbsp; Never asked for this</p>
 						<nav>
 							<ul>
